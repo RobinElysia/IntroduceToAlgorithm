247db1e3744858e6ad312ff2ba0a80f46012362e
qwp_p <qwp20060309@outlook.com>
2026-05-29 12:40:08 +0800

    new al

---
+++ src/day61_90/day71/Main.java
@@ -0,0 +1,44 @@
+package day61_90.day71;
+
+import java.util.Stack;
+
+/**
+ * @author RobinElysia
+ * @version 1.0
+ */
+public class Main {
+}
+
+class MinStack {
+    private Stack<Integer> stack;      // 主栈
+    private Stack<Integer> minStack;   // 辅助栈，存储最小值
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // 如果辅助栈为空或当前值小于等于辅助栈顶，则压入辅助栈
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        // 如果弹出的值等于当前最小值，辅助栈也要弹出
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
