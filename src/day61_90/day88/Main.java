dac625fc73685bd94647b5dbaf4abf866ed88591
qwp_p <qwp20060309@outlook.com>
2026-06-03 21:38:16 +0800

    new al

--- src/day61_90/day88/Main.java
+++ src/day61_90/day88/Main.java
@@ -15,17 +15,17 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
         // dp[i] 表示 s[0..i-1] 能否被拆分
-        boolean[] dp = new boolean[n + 1];
+        boolean[] dp = new boolean[n + 1]; // 长度为 n+1，dp[i] 对应 s[0..i-1]
         dp[0] = true; // 空串可被拆分
 
         // 用 HashSet 加速查找，O(1) 判断单词是否在字典中
-        Set<String> wordSet = new HashSet<>(wordDict);
+        Set<String> wordSet = new HashSet<>(wordDict); // 将 wordDict 转换为 HashSet，提升查找效率
 
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 // 如果 s[0..j-1] 可被拆分，且 s[j..i-1] 在字典中
-                if (dp[j] && wordSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
+                if (dp[j] && wordSet.contains(s.substring(j, i))) { // s.substring(j, i) 获取 s[j..i-1]
+                    dp[i] = true; // s[0..i-1] 可被拆分
                     break; // 找到一个可行拆分即可，提前退出
                 }
             }
